96ed679347d1c4a193a13301a3ecd11c25b6bc45
Bertil <ipsen.bertil@gmail.com>
2025-05-15 23:19:57 +0200

    update home tab icon name

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -48,7 +48,7 @@ export default function TabLayout() {
             title: t("home"),
             tabBarIcon: ({ color, focused }) => (
               <Ionicons
-                name={focused ? "home-sharp" : "home-outline"}
+                name={focused ? "home" : "home-outline"}
                 color={color}
                 size={24}
               />
